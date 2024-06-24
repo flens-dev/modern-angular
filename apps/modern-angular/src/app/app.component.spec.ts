f2a06b695d045515c7d0f4897d6dc07845696638
Lars Hanisch <dvb@flensrocker.de>
2024-06-24 19:35:44 +0200

    Remove Nx-Welcome Component

--- apps/modern-angular/src/app/app.component.spec.ts
+++ apps/modern-angular/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      providers: [provideRouter([])],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome modern-angular'
+      'Modern Angular'
     );
   });
 
