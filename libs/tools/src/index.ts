e11c464bed47e2df42f5ff73274de234adf3a538
Lars Hanisch <dvb@flensrocker.de>
2024-10-13 16:26:08 +0200

    tools - add fromEventToObservable

--- libs/tools/src/index.ts
+++ libs/tools/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/forms';
+export * from './lib/from-event-to-observable';
 export * from './lib/service-call';
 export * from './lib/value-source';
---
+++ libs/tools/src/lib/from-event-to-observable.ts
@@ -0,0 +1,22 @@
+import { ElementRef } from '@angular/core';
+
+import { NEVER, Observable, fromEvent, switchMap } from 'rxjs';
+
+import { ValueSource, sourceToObservable } from './value-source';
+
+/**
+ * Returns an observable which emits the events of the given element.
+ * Undefined/null will be mapped to the NEVER obsrevable.
+ *
+ * Usefull to declaratively subscribe to elements retrieved with `viewChild` from a template.
+ */
+export const fromEventToObservable = <TEvent extends Event = Event>(
+  $elementRef$: ValueSource<ElementRef | null | undefined>,
+  eventName: string
+): Observable<TEvent> => {
+  return sourceToObservable($elementRef$).pipe(
+    switchMap((el) =>
+      el == null ? NEVER : fromEvent<TEvent>(el.nativeElement, eventName)
+    )
+  );
+};
