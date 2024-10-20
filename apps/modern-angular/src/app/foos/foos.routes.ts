59a6676c0baec379362df225821e45371d38cdec
Lars Hanisch <dvb@flensrocker.de>
2024-10-20 12:49:05 +0200

    CRUD example - refactored routes

--- apps/modern-angular/src/app/foos/foos.routes.ts
+++ apps/modern-angular/src/app/foos/foos.routes.ts
@@ -3,32 +3,30 @@ import { Routes } from '@angular/router';
 import { provideFooServices } from './model';
 import { provideFooInMemoryRepository } from './infrastructure';
 
-const FOO_CHILD_ROUTES: Routes = [
-  {
-    path: 'create',
-    loadComponent: () =>
-      import('./foo-create.component').then(
-        ({ FooCreateComponent }) => FooCreateComponent,
-      ),
-  },
-  {
-    path: ':fooId/edit',
-    loadComponent: () =>
-      import('./foo-edit.component').then(
-        ({ FooEditComponent }) => FooEditComponent,
-      ),
-  },
-  {
-    path: '',
-    loadComponent: () =>
-      import('./foos.component').then(({ FoosComponent }) => FoosComponent),
-  },
-];
-
 export const FOO_ROUTES: Routes = [
   {
     path: '',
     providers: [provideFooInMemoryRepository(), provideFooServices()],
-    children: FOO_CHILD_ROUTES,
+    children: [
+      {
+        path: 'create',
+        loadComponent: () =>
+          import('./foo-create.component').then(
+            ({ FooCreateComponent }) => FooCreateComponent,
+          ),
+      },
+      {
+        path: ':fooId/edit',
+        loadComponent: () =>
+          import('./foo-edit.component').then(
+            ({ FooEditComponent }) => FooEditComponent,
+          ),
+      },
+      {
+        path: '',
+        loadComponent: () =>
+          import('./foos.component').then(({ FoosComponent }) => FoosComponent),
+      },
+    ],
   },
 ];
