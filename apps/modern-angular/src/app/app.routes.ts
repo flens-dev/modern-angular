3f9f3111f6ebc427de9086eac358c4f2b9ad333a
Lars Hanisch <dvb@flensrocker.de>
2024-10-21 13:40:29 +0200

    remove outdated example

--- apps/modern-angular/src/app/app.routes.ts
+++ apps/modern-angular/src/app/app.routes.ts
@@ -5,8 +5,4 @@ export const appRoutes: Route[] = [
     path: 'foos',
     loadChildren: () => import('./foos').then((m) => m.FOO_ROUTES),
   },
-  {
-    path: 'typical',
-    loadChildren: () => import('./typical').then((m) => m.AppTypicalModule),
-  },
 ];
