b67c7e8925018085abc5194f26ce515de6b97c7f
Lars Hanisch <dvb@flensrocker.de>
2024-12-01 13:00:52 +0100

    tools - refactored AbortOnDestroyRef

--- libs/tools/src/lib/abort-on-destroy.ts
+++ libs/tools/src/lib/abort-on-destroy.ts
@@ -1,17 +1,32 @@
-import { DestroyRef, InjectionToken, Provider } from '@angular/core';
+import { DestroyRef, inject, InjectionToken, Provider } from '@angular/core';
+
+const createAbortOnDestroy = (destroyRef: DestroyRef): AbortSignal => {
+  const abort = new AbortController();
+  destroyRef.onDestroy(() => abort.abort());
+  return abort.signal;
+};
 
 export const AbortOnDestroyRef = new InjectionToken<AbortSignal>(
   'AbortOnDestroyRef',
 );
 
+/**
+ * Registers an AbortOnDestroyRef in dependecy injection.
+ */
 export const provideAbortOnDestroyRef = (): Provider[] => [
   {
     provide: AbortOnDestroyRef,
-    useFactory: (destroyRef: DestroyRef) => {
-      const abort = new AbortController();
-      destroyRef.onDestroy(() => abort.abort());
-      return abort.signal;
-    },
+    useFactory: createAbortOnDestroy,
     deps: [DestroyRef],
   },
 ];
+
+/**
+ * Uses an AbortOnDestroyRef if provided in dependency injection.
+ * Otherwise creates a new AbortController and link it to the DestroyRef.
+ * @returns an AbortSignal which aborts when the DestroyRef is destroyed.
+ */
+export const injectAbortOnDestroy = (): AbortSignal => {
+  const signal = inject(AbortOnDestroyRef, { optional: true });
+  return signal == null ? createAbortOnDestroy(inject(DestroyRef)) : signal;
+};
